d91b98609d7da99cf5fcdbdf9d1061d6a62deca2
Paul Dib <paul.dib@orange.fr>
2015-08-02 22:54:42 -0700

    Initial commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include <SFML/Window.hpp>
+
+
+int main(int argc, char** argv) {
+  sf::Window window(sf::VideoMode(800, 600), "My window");
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+	window.close();
+      }
+    }     
+  }
+  return 0;
+}
